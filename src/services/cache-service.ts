87b70a79c5741cf566a2214c6262777fd1d1870d
Oldemar Jesus <oldemego@gmail.com>
2024-04-14 09:12:45 +0100

    change cache dir

--- src/services/cache-service.ts
+++ src/services/cache-service.ts
@@ -1,8 +1,11 @@
 import Cache from 'file-system-cache';
+import cachedir from 'cachedir'
 
 export default function () {
+    const cachedirs = cachedir("simar")
+    console.info("Storing cache at " + cachedirs)
     return Cache({
-        basePath: "./.cache", // (optional) Path where cache files are stored (default).
+        basePath: cachedirs + "/.cache", // (optional) Path where cache files are stored (default).
         ns: "my-namespace",   // (optional) A grouping namespace for items.
         hash: "sha1",          // (optional) A hashing algorithm used within the cache key.
         ttl: 3500               // (optional) A time-to-live (in secs) on how long an item remains cached.
